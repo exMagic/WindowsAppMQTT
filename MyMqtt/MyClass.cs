fe067d77a560bf5e839547a5644eb615167fb8b4
Maciek Anuszkiewicz <maciek.anuszkiewicz@gmail.com>
2018-08-26 06:38:26 +0200

    Add project files.

---
+++ MyMqtt/MyClass.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace MyMqtt
+{
+    public class MyMqttClass
+
+    {
+        public MqttClient mqttClient;
+        public bool ConnectMe()
+        {
+            mqttClient = new MqttClient("m21.cloudmqtt.com", 12004, false, null, null, MqttSslProtocols.None);
+            mqttClient.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+
+            mqttClient.Connect("PCname", "uugymqbo", "pSUnBCfynYB8", false, 9999);
+            if (mqttClient.IsConnected)
+            {
+                return true;
+            }
+            else
+                return false;
+
+        }
+        //mqttClient.Subscribe(new string[] { topic }, new byte[] { 0 });
+
+        //mqttClient.Publish(topic, System.Text.Encoding.UTF8.GetBytes("jest z kosoli"));
+
+
+        public bool Send(string msg)
+        {
+            string topic = "b";
+            mqttClient.Publish(topic, System.Text.Encoding.UTF8.GetBytes(msg));
+
+            return true;
+        }
+        public string resivedMsg;
+        public string Sub()
+        {
+            string topic = "b";
+            mqttClient.Subscribe(new string[] { topic }, new byte[] { 2 });
+
+            return "resivedMsg";
+
+        }
+
+        public void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            string ReceivedMessage = Encoding.UTF8.GetString(e.Message);
+            resivedMsg = ReceivedMessage;
+
+        }
+
+        public string checkMsg()
+        {
+            return resivedMsg;
+        }
+
+
+    }
+}
+
---
+++ WindowsAppMQTT/Form1.vb
@@ -0,0 +1,27 @@
+﻿Imports MyMqtt
+
+Public Class Form1
+
+
+    Dim mm As MyMqttClass = New MyMqttClass
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label2.Text = mm.ConnectMe().ToString()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim isBup = mm.Send(TextBox1.Text)
+        If isBup Then
+            Label1.Text = "poszlo"
+        End If
+        'Label1.Text = mm.Start().ToString()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Label3.Text = mm.checkMsg
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Label4.Text = mm.checkMsg
+    End Sub
+End Class
