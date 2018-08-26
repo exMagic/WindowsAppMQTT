287f21c79adfa160e192ec51e1160b1f2b93f389
Maciek Anuszkiewicz <maciek.anuszkiewicz@gmail.com>
2018-08-26 19:52:16 +0200

    table from remote server

--- WindowsAppMQTT/Form1.Designer.vb
+++ WindowsAppMQTT/Form1.Designer.vb
@@ -31,6 +31,21 @@ Partial Class Form1
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.txtBarcode = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtAntall = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.lblMottat = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.lblBestilt = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.BindingSource1 = New System.Windows.Forms.BindingSource(Me.components)
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BindingSource1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Button1
@@ -97,11 +112,126 @@ Partial Class Form1
         'Timer1
         '
         '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(14, 378)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(863, 150)
+        Me.DataGridView1.TabIndex = 7
+        '
+        'txtBarcode
+        '
+        Me.txtBarcode.Location = New System.Drawing.Point(577, 47)
+        Me.txtBarcode.Name = "txtBarcode"
+        Me.txtBarcode.Size = New System.Drawing.Size(300, 20)
+        Me.txtBarcode.TabIndex = 8
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(525, 50)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(46, 13)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "barcode"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(516, 76)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(55, 13)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "antall/kolli"
+        '
+        'txtAntall
+        '
+        Me.txtAntall.Location = New System.Drawing.Point(577, 73)
+        Me.txtAntall.Name = "txtAntall"
+        Me.txtAntall.Size = New System.Drawing.Size(300, 20)
+        Me.txtAntall.TabIndex = 10
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(516, 112)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(31, 13)
+        Me.Label7.TabIndex = 12
+        Me.Label7.Text = "navn"
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Location = New System.Drawing.Point(574, 112)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(12, 13)
+        Me.lblName.TabIndex = 13
+        Me.lblName.Text = "x"
+        '
+        'lblMottat
+        '
+        Me.lblMottat.AutoSize = True
+        Me.lblMottat.Location = New System.Drawing.Point(574, 137)
+        Me.lblMottat.Name = "lblMottat"
+        Me.lblMottat.Size = New System.Drawing.Size(12, 13)
+        Me.lblMottat.TabIndex = 15
+        Me.lblMottat.Text = "x"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(516, 137)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(36, 13)
+        Me.Label9.TabIndex = 14
+        Me.Label9.Text = "mottat"
+        '
+        'lblBestilt
+        '
+        Me.lblBestilt.AutoSize = True
+        Me.lblBestilt.Location = New System.Drawing.Point(574, 165)
+        Me.lblBestilt.Name = "lblBestilt"
+        Me.lblBestilt.Size = New System.Drawing.Size(12, 13)
+        Me.lblBestilt.TabIndex = 17
+        Me.lblBestilt.Text = "x"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(516, 165)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(34, 13)
+        Me.Label10.TabIndex = 16
+        Me.Label10.Text = "bestilt"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(577, 197)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 18
+        Me.Button3.Text = "Show"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(979, 540)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.lblBestilt)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.lblMottat)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.lblName)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.txtAntall)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtBarcode)
+        Me.Controls.Add(Me.DataGridView1)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Label3)
@@ -111,6 +241,8 @@ Partial Class Form1
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BindingSource1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -124,4 +256,17 @@ Partial Class Form1
     Friend WithEvents Button2 As Button
     Friend WithEvents Label4 As Label
     Friend WithEvents Timer1 As Timer
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents txtBarcode As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtAntall As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents lblName As Label
+    Friend WithEvents lblMottat As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents lblBestilt As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents BindingSource1 As BindingSource
 End Class
--- WindowsAppMQTT/Form1.vb
+++ WindowsAppMQTT/Form1.vb
@@ -1,12 +1,33 @@
 ﻿Imports MyMqtt
+Imports MySql.Data.MySqlClient
+Imports System.Configuration
+
+
 
 Public Class Form1
 
 
     Dim mm As MyMqttClass = New MyMqttClass
 
+    Public ReadOnly Property ConnectionString As String
+        Get
+            Return ConfigurationManager.ConnectionStrings("ConnectionString").ToString()
+        End Get
+    End Property
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Label2.Text = mm.ConnectMe().ToString()
+        DataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect
+        'SQL
+        Dim connetcion As New MySqlConnection(ConnectionString)
+        Dim table As New DataTable()
+        Dim adapter As New MySqlDataAdapter("SELECT * FROM ewastepk_mg.vare", connetcion)
+
+        adapter.Fill(table)
+
+        BindingSource1.DataSource = table
+        DataGridView1.DataSource = BindingSource1
+
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
@@ -23,5 +44,20 @@ Public Class Form1
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
         Label4.Text = mm.checkMsg
+    End Sub
+    Dim index
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        For Each row As DataGridViewRow In DataGridView1.Rows
+            Dim barcode = DataGridView1.Item(1, row.Index).Value
+            Dim antallKolli = DataGridView1.Item(5, row.Index).Value
+            If barcode = txtBarcode.Text And antallKolli = txtAntall.Text Then
+                index = row.Index
+            End If
+        Next
+
+        lblName.Text = DataGridView1.Item(2, index).Value
+        lblMottat.Text = DataGridView1.Item(3, index).Value
+        lblBestilt.Text = DataGridView1.Item(4, index).Value
+
     End Sub
 End Class
